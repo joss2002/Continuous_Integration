e64d2ca2a5baf0ab9a93006f5cd541dc2d7acfba
Erik Olsson <eolsson5@kth.se>
2026-02-12 14:41:34 +0100

    Remove unneeded import

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,7 +2,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
